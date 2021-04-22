684e1f75247fa9bfd58c1798b97ae6baa76276b5
Mina Dawood <mina.latif@live.com>
2021-04-22 19:41:27 -0400

    added cart screen

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,10 +1,11 @@
 import { compose, createStore, applyMiddleware, combineReducers } from 'redux'
 import thunk from 'redux-thunk'
-import { productListReducer } from './reducers/productReducers';
+import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 
 const initialState = {}
 const reducer = combineReducers({
     productList: productListReducer,
+    productDetails: productDetailsReducer
 })
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
